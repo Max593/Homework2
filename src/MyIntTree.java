8044a48c4516541216c57e887c3a9c4b196a7eb7
max <maxmolotov@hotmail.it>
2017-03-22 19:05:45 +0100

    Initial commit

---
+++ src/MyIntTree.java
@@ -0,0 +1,53 @@
+/**
+ * Created by max on 22/03/17.
+ */
+public class MyIntTree implements IntTree {
+    /*Inserire costruttore*/
+
+    @Override
+    public int getValue() {  //Da completare
+        return 0;
+    }
+
+    @Override
+    public int childrenNumber() {  //Da completare
+        return 0;
+    }
+
+    @Override
+    public int nodes() {  //Da completare
+        return 0;
+    }
+
+    @Override
+    public int height() {  //Da completare
+        return 0;
+    }
+
+    @Override
+    public boolean equals(IntTree t) {  //Da completare
+        return false;
+    }
+
+    @Override
+    public void addChild(IntTree child) {  //Da completare
+
+    }
+
+    @Override
+    public IntTree followPath(int[] path) throws NoSuchTreeException {  //Da completare
+        return null;
+    }
+
+    @Override
+    public void search() {  //Da completare
+
+    }
+
+    @Override
+    public void printIntTree() {  //Da completare
+
+    }
+
+    /* Probabilmente manca visit() */
+}
---
+++ src/NoSuchTreeException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by max on 22/03/17.
+ */
+public class NoSuchTreeException extends Exception {
+}
